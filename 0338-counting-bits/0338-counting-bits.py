1ef54521cc53096378beafc9dce67cb0b59597d5
Rémi Brenaut <124099693+remicku@users.noreply.github.com>
2025-05-21 03:25:19 +0200

    Time: 7 ms (77.74%), Space: 17.9 MB (11.58%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -4,14 +4,14 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        ans = [0] * (n + 1)
+        ans = [0]
         prev = 1
 
         for i in range(1, n + 1):
             if i == prev * 2:
                 prev = i
 
-            ans[i] = 1 + ans[i - prev]
+            ans.append(1 + ans[i - prev])
 
         return ans
         
