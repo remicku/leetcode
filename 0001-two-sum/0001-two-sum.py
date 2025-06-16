7c328f5081d117a9fb0470e62a53fabe54e77db4
Rémi Brenaut <124099693+remicku@users.noreply.github.com>
2025-06-17 01:40:41 +0200

    Time: 0 ms (100%), Space: 13.3 MB (30.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,10 +6,12 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        for i in range(n):
-            for j in range (i + 1, n):
-                if (nums[i] + nums[j] == target):
-                    return [i, j]
-
-        return []
+        hashmap = {}
         
+        for i in range(n):
+            complement = target - nums[i]
+
+            if complement in hashmap:
+                return [hashmap[complement], i]
+
+            hashmap[nums[i]] = i
